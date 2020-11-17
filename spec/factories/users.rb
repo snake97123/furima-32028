af2e56329cf7b607ded887a680be8aaeed836710
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-17 16:59:59 +0900

    ユーザー管理機能

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"test"}
+    email                 {"test@example"}
+    password              {"000000"}
+    password_confirmation {password}
+    
+  end
+end

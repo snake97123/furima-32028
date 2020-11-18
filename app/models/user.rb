d0d2c09495f2009118813be1899e922699009f6a
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-18 18:01:15 +0900

    フリガナ部分のバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ with_options presence: true do
   with_options format: {with: /\A[ぁ-んァ-ン一-龥]+\z/,} do 
     validates   :family_name
     validates   :first_name
+  end
+  with_options format: {with:/\A([ァ-ン]|ー)+\z/,} do
     validates   :family_name_kana
     validates   :first_name_kana
   end

e77f4608174be779eb48344df9e75885b877af81
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-20 17:56:05 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,30 +28,30 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Description can't be blank")
     end
-    it 'カテゴリーが入力されていないと登録できないこと' do
-      @item.category_id = nil
+    it 'カテゴリーが1では登録できないこと' do
+      @item.category_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include('Category is not a number')
+      expect(@item.errors.full_messages).to include('Category must be other than 1')
     end
-    it '商品の状態が入力されていないと登録できないこと' do
-      @item.status_id = nil
+    it '商品の状態が1では登録できないこと' do
+      @item.status_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include('Status is not a number')
+      expect(@item.errors.full_messages).to include('Status must be other than 1')
     end
-    it '配送料の負担が入力されていないと登録できないこと' do
-      @item.cost_id = nil
+    it '配送料の負担が1では登録できないこと' do
+      @item.cost_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include('Cost is not a number')
+      expect(@item.errors.full_messages).to include('Cost must be other than 1')
     end
-    it '発送元の地域が入力されていないと登録でないこと' do
-      @item.prefecture_id = nil
+    it '発送元の地域が1では登録でないこと' do
+      @item.prefecture_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include('Prefecture is not a number')
+      expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
     end
-    it '発送までの日数が入力されていないと登録できないこと' do
-      @item.day_id = nil
+    it '発送までの日数が1では登録できないこと' do
+      @item.day_id = 1
       @item.valid?
-      expect(@item.errors.full_messages).to include('Day is not a number')
+      expect(@item.errors.full_messages).to include('Day must be other than 1')
     end
     it '価格が空だと登録できないこと' do
       @item.price = nil
@@ -64,12 +64,12 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Price is not a number')
     end
     it '価格が300円以下では登録できないこと' do
-      @item.price = '200'
+      @item.price = 200
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
     it '価格が9999999円以上では登録できないこと' do
-      @item.price = '10000000'
+      @item.price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end

46cb8d9a9e1c7c265f27569908a5d12e784170d6
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-21 19:54:12 +0900

    商品詳細機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :day
   has_one_attached :image
 
   validates :price, presence: true, format: { with: /\A^[0-9]+$\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }

6128f1b07e299eb6b3d38285a389b7cb04b00e92
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-21 11:08:49 +0900

    商品一覧表示機能

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,7 +69,7 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
     end
     it '価格が9999999円以上では登録できないこと' do
-      @item.price = 10000000
+      @item.price = 10_000_000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end

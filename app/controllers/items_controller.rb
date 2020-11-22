c36a4927afb4fb9b4b3e5f00a59f5c9b738e1a51
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-22 11:21:17 +0900

    商品情報編集機能　コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,7 +39,7 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :price, :image, :description, :category_id, :status_id, :cost_id, :prefecture_id, :day_id).merge(user_id: current_user.id)
   end
-  
+
   def set_item
   @item = Item.find(params[:id])
   end

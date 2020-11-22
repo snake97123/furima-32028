f99366cbffac6047a7d3baa0c8ba5bfc7fcbfa8e
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-22 11:45:07 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit]
 
   def index
@@ -34,6 +34,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
 
   def item_params
@@ -41,6 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def set_item
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 end

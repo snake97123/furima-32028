a0a9b2f9000512f389f834b5c96a46f2bd50c2cb
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-22 14:05:05 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,12 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user_id
-      @item.destroy
+   @item.destroy if current_user.id == @item.user_id
       redirect_to root_path
-    else
-      render :edit
-    end
   end
 
   private

d8126a60094274fd9940e35aa29d1fddfc244504
snake97123 <73522198+snake97123@users.noreply.github.com>
2020-11-26 17:05:53 +0900

    クレジットカード決済機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,6 +8,7 @@ class PurchasesController < ApplicationController
      @item = Item.find_by(params[:id])
      @purchase_card = PurchaseCard.new(card_params)
      if @purchase_card.valid?
+      pay_item
       @purchase_card.save
       redirect_to root_path
      else
@@ -18,7 +19,16 @@ class PurchasesController < ApplicationController
   private
 
   def card_params
-    params.require(:purchase_card).permit(:post_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(user_id:current_user.id, item_id:@item.id)
+    params.require(:purchase_card).permit(:post_code, :prefecture_id, :city, :block, :building_name, :phone_number).merge(user_id:current_user.id, item_id:@item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create( 
+       amount: @item.price,
+       card: card_params[:token],
+       currency: 'jpy'
+    )
   end
 end
 
--- app/models/purchase_card.rb
+++ app/models/purchase_card.rb
@@ -1,6 +1,6 @@
 class PurchaseCard
   include ActiveModel::Model
-  attr_accessor    :item_id, :user_id, :post_code, :prefecture_id, :city, :block, :building_name, :phone_number,  :purchase_id
+  attr_accessor    :item_id, :user_id, :post_code, :prefecture_id, :city, :block, :building_name, :phone_number,  :purchase_id, :token
 
   with_options presence: true do
     validates  :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,6 +8,7 @@ class PurchaseCard
     validates  :block
     validates  :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates  :phone_number, format: {with: /\A^(0{1}\d{9,10})$\z/}
+    validates  :token
   end
   
   def save
